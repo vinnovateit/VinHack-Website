192e33543b2cb8b93db56b12807024ec2091db88
Arpit Jain <arpitkotecha1999@gmail.com>
2019-03-20 12:52:29 +0530

    image size reduced and custom.js moved to main.js

--- assets/js/custom.js
+++
@@ -1,5 +0,0 @@
-var h = new Date().getHours();
-var date = new Date().getDate();
-if (date <= 22 || date >= 24) {
-    document.getElementById('live-update-btn').style.display = "none";
-}
--- assets/js/main.js
+++ assets/js/main.js
@@ -74,3 +74,9 @@
         }, breakpoints.on("<=medium", i), breakpoints.on(">medium", o)
     }), n("#banner")._parallax()
 }(jQuery);
+
+var h = new Date().getHours();
+var date = new Date().getDate();
+if (date <= 22 || date >= 24) {
+    document.getElementById('live-update-btn').style.display = "none";
+}
