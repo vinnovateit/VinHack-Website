e876dee27024658bee2ab736d9e51dcb334adfb6
kvssankar <55633494+kvssankar@users.noreply.github.com>
2020-02-22 10:37:12 +0530

    made changes

--- assets/js/registerButton.js
+++ assets/js/registerButton.js
@@ -7,9 +7,10 @@ const firstlayer=document.querySelector('#q1');
 console.log(button);
 const t1=new TimelineLite({paused:true});
 
-t1.fromTo('#q1',1,{
+t1.to('#banner',.5,{filter:"blur(5px)"},'-=.85').fromTo('#q1',1,{
     opacity: 0,
     scale:2,
+    clipPath:"circle(0% at 50% 50%)",
     ease: "Power2.out"
 },{
     scale:1
@@ -32,28 +33,30 @@ var f;
 var before;
 var after;
 function nextpage(){
-    if(i<10){
+    console.log(i);
+    if(i<7){
         f="q"+i;
         before=document.getElementById(f);
-        // before.style.display="none";
-          t2.fromTo(before,1,{
-               display:"flex",
-               y:0,
-              opacity:1,
-               ease: "Power2.out"
-          },{
-               scale:1
-           })
-           .to(before,1,{
-               display:"none",
-              y:-500,
-               opacity:0,
-               ease: "Power2.out"
-           },'-=.85');
-           t2.play();
+        before.style.display="none";
+            // t2
+            // .fromTo(before,1,{
+                 
+            //      clipPath:"circle(100% at 50% 50%)",
+            //     opacity:1,
+            //      ease: "Power2.out"
+            // },{
+            //      scale:1
+            //  })
+            //  .to(before,1,{
+            //      display:"none",
+            //      clipPath:"circle(0% at 50% 50%)",
+            //      opacity:0,
+            //      ease: "Power2.out"
+            //  },'-=.85');
+             t2.play();
     }
     console.log(i);
-    if(i<11){
+    if(i<8){
         i++;
         console.log(i);
         f="q"+i;
@@ -61,7 +64,24 @@ function nextpage(){
         after.style.display="flex";
         console.log(after.style.display);
     }
+    console.log(i);
+    if(i==7){
+        i=1;
+        document.querySelector('#banner').style.filter="blur(0)";
+    }
 }
+
+$(document).mouseup(function(e) 
+{
+    var container = $(".layer");
+
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {
+        document.querySelector('#banner').style.filter="blur(0)";
+        container.hide();
+    }
+});
 
 
 
